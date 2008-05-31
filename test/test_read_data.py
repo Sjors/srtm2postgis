a564c8a3770f32a853ba6a5c96ab3501be3f02f4
sjors <sjors@b9d5c4c9-76e1-0310-9c85-f3177eceb1e4>
2008-05-31 05:29:15 +0000

    Refactored test_read_data.py for GDAL.
    Used GDAL [1] to import the SRTM data. Refactored test/test_read_data.py
    It turns out my original method produced different (likely incorrect)
    altitudes. Changed the test to reflect this, but it has not been
    verified independantly.
    
    I split the Melbourne test in 2 seperate tests.
    
    Added two .git ignore files to ignore vim swap files and the commit
    message file.
    
    [1] http://trac.osgeo.org/gdal/wiki/GdalOgrInPython
    
    
    git-svn-id: http://svn.openstreetmap.org/applications/utils/srtm2postgis/trunk@8010 b9d5c4c9-76e1-0310-9c85-f3177eceb1e4

--- test/test_read_data.py
+++ test/test_read_data.py
@@ -1,55 +1,43 @@
-from struct import unpack
 import unittest
+from osgeo import gdal, gdal_array
 
 class TestImportScript(unittest.TestCase):
     def testPointsPerTile(self):
-			file = open('../data/Australia/S11E119.hgt','rb')
-			b = file.read()
-			self.assertEqual(len(b)/2,1201*1201)
-			file.close()
+      data = loadTile('S11E119')
+      self.assertEqual(data.size,1201*1201)
     
     def testFirstNumber(self):
-      file = open('../data/Australia/S11E119.hgt','rb')
-      be_int = file.read(4)
-      le_int = unpack('>i',be_int)
-      self.assertEqual(le_int[0],0)
-      file.close()
+      data = loadTile('S11E119')
+      self.assertEqual(data[0][0],0)
 
     def testTileIntegrity(self):
-      # Uses two adjecant tiles around Melbourne to test
-      # * the sum of altitudes of all 1201*1201 points in the tiles
-      # * Eastern edge of West Melbourne tile should be the same as western edge 
-      # of East melbourne tile.
-      
-      # Melbourne (West):
-      res = loadTile("S37E144.hgt")
-      self.assertEqual(res[1],14632703885312)
-      west = res[0]
+      # The sum of altitudes of all 1201*1201 points in two tiles
+     
+       # Melbourne (West):
+      west = loadTile("S37E144")
+      self.assertEqual(west.sum(),223277342)
 
       ## Melbourne (East):
-      res = loadTile("S37E145.hgt")
-      self.assertEqual(res[1], 17769215361024)
-      east=res[0]
+      east = loadTile("S37E145")
+      self.assertEqual(east.sum(), 271136709)
+    
+    def testTileOverlap(self):
+      # Uses two adjecant tiles around Melbourne to test
+      # * Eastern edge of West Melbourne tile should be the same as 
+      #   western edge of East Melbourne tile.
+      
+      # Melbourne (West):
+      west = loadTile("S37E144")
+
+      ## Melbourne (East):
+      east = loadTile("S37E145")
 
       for i in range(1201):
         self.assertEqual(west[i][1200] - east[i][0],0) 
 
 def loadTile(filename):
-  file = open('../data/Australia/' + filename,'rb')
-  tile = []
-  total = 0
-  for x in range(1201):
-    row = []
-    for y in range(1201):
-      # Add two bytes for some reason (x86_64 specific?)
-      be_int = file.read(2) + '\x00\x00' 
-      row.append(unpack('>i',be_int)[0])
-
-    tile.append(row)
-    total = total + sum(row)
-
-  file.close
-  return [tile, total]
+  srtm = gdal.Open('../data/Australia/' + filename + '.hgt')
+  return gdal_array.DatasetReadAsArray(srtm)
 
 if __name__ == '__main__':
     unittest.main()
